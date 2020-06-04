3e5095663a1d3f758a011a48f828b7fbef0b1286
kushal <kushal@04.com>
2020-06-04 22:37:04 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Layout/Navbar/Navbar";
+import Footer from "./components/Layout/Footer/Footer";
+import { Switch, Route } from "react-router-dom";
+import Home from "./components/Pages/Home";
+import CreateEvent from "./components/Pages/CreateEvent/CreateEvent";
+import login from "./components/Pages/login";
+import AboutUs from "./components/Pages/About/AboutUs";
+import Contacts from "./components/Pages/ContactUs";
+import Detalis from "./components/Pages/Details";
+import NotFoundPage from "./components/Pages/NotFoundPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={AboutUs} />
+        <Route path="/contacts" component={Contacts} />
+        <Route path="/create" component={CreateEvent} />
+        <Route path="/details" component={Detalis} />
+        <Route path="/login" component={login} />
+
+        <Route component={NotFoundPage} />
+      </Switch>
+      <Footer />
     </div>
   );
 }
