85d50ac3a89a1a8bfe52b05f8dbdd7d71d24c6ff
kushal <kushal@04.com>
2020-06-04 23:01:22 +0530

    Updates

---
+++ precache-manifest.6e9b7ff49f52126611557b9cf5bc09da.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "387c41c20ed293d860a568b954fb1392",
+    "url": "/findmyevents/index.html"
+  },
+  {
+    "revision": "5a878d4048682fc2ac7d",
+    "url": "/findmyevents/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "23d27efcdaf3aa0b4333",
+    "url": "/findmyevents/static/css/main.7114af0d.chunk.css"
+  },
+  {
+    "revision": "5a878d4048682fc2ac7d",
+    "url": "/findmyevents/static/js/2.d54752f8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/findmyevents/static/js/2.d54752f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23d27efcdaf3aa0b4333",
+    "url": "/findmyevents/static/js/main.4cdf2d61.chunk.js"
+  },
+  {
+    "revision": "a8ba122d5a3dc8fac1d9",
+    "url": "/findmyevents/static/js/runtime-main.4277ab44.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/findmyevents/static/media/logo.5d5d9eef.svg"
+  }
+]);
